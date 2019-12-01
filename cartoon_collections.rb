a8e49d30b084cadb0a3e829fa3365efe10a4b578
Erik Kasperson <chefkasperson@gmail.com>
2019-12-01 08:41:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,8 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.map {|a| 
-  if a.length > 4}
+ if array.map {|a| 
+  a.length > 4}
     return TRUE
   else
     return FALSE
