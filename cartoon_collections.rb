518b4b8c9b50fa280e6ee5e707c31e09ad6bee5b
Erik Kasperson <chefkasperson@gmail.com>
2019-12-01 09:03:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,12 @@ end
 def find_the_cheese(array)
   
   cheese_types = ["cheddar", "gouda", "camembert"]
-  i -
-  array.include?("cheddar")
+  i = 0 
+  if
+  array.include?(cheese_types[i])
+  cheese_types[i]
+else
+  nil 
+end
 
 end
